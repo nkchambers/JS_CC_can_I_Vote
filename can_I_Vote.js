b6ff9122793d1d8efa9a3d44372779fa5ee0dd49
nkchambers <nkchames@gmail.com>
2024-01-29 22:11:03 -0800

    adding can_I_Vote for 1-29-24

---
+++ can_I_Vote.js
@@ -0,0 +1,26 @@
+//CODECADEMY JAVASCRIPT CODING CHALLENGES - can_I_Vote challenge
+
+// STARTING CODEBASE
+// Write your function here:
+const canIVote = number => {
+    if (number > 17) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Testing canIVote function
+console.log(canIVote(25));
+console.log(canIVote(17));
+console.log(canIVote(58));
+console.log(canIVote(15));
+
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+// console.log(canIVote(19)) // Should print true
+
+// We encourage you to add more function calls of your own to test your code!
