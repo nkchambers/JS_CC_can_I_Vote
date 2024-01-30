38da9edb324405c97ef2cc8f9c1e76548458e566
nkchambers <nkchames@gmail.com>
2024-01-29 22:31:39 -0800

    adding Task 1 desc

--- can_I_Vote.js
+++ can_I_Vote.js
@@ -1,6 +1,11 @@
 //CODECADEMY JAVASCRIPT CODING CHALLENGES - can_I_Vote challenge
 
 // STARTING CODEBASE
+
+//Task 1 - The most common minimum age to vote is 18. 
+// Write a function canIVote() that takes in a number, representing the person’s age, 
+// and returns the boolean true if they are 18 years old or older, and the boolean false if they are not.
+
 // Write your function here:
 const canIVote = number => {
     if (number > 17) {
